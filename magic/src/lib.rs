2ecd86a5b56ec7da9e4b1b5e558936d1394ccb7f
Void-CA <castilloari282@gmail.com>
2026-02-21 23:04:48 -0600

    test(database): switch to persistent SQLite database for testing

--- magic/src/lib.rs
+++ magic/src/lib.rs
@@ -1,1 +1,9 @@
 pub use magic_derive::MagicModel;
+use sqlx::SqlitePool;
+#[derive(MagicModel)]
+#[magic(table = "users")]
+pub struct User {
+    pub id: i64,
+    pub name: String,
+    pub email: String,
+}
--- magic/src/main.rs
+++ magic/src/main.rs
@@ -11,12 +11,12 @@ pub struct User {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // Base de datos en memoria
-    let pool = SqlitePool::connect(":memory:").await?;
+    // Base de datos en disco (archivo "test.db")
+    let pool = SqlitePool::connect("sqlite://test.db").await?;
 
-    // Crear tabla
+    // Crear tabla si no existe
     sqlx::query(
-        "CREATE TABLE users (
+        "CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             email TEXT NOT NULL
@@ -26,14 +26,17 @@ async fn main() -> anyhow::Result<()> {
     .await?;
 
     // Instancia de NewUser
-    let new_user = User::new("Alice".into(), "alice@example.com".into());
+    let user_1 = User::new("Alice".into(), "alice@example.com".into());
+    let user_2 = User::new("Bob".into(), "bob@example.com".into());
+    let user_3 = User::new("Charlie".into(), "charlie@example.com".into());
 
     // Insertar
-    let id = new_user.insert(&pool).await?;
-
+    let id = user_1.insert(&pool).await?;
+    user_2.insert(&pool).await?;
+    user_3.insert(&pool).await?;
     println!("User inserted with id: {}", id);
 
-
+    // Actualizar
     let new_put_user = User::new("Alice".into(), "alice.updated@example.com".into());
     let rows_affected = new_put_user.update(&pool, id).await?;
     println!("User updated with rows affected: {}", rows_affected);
