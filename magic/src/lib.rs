193d640ca9007ab0aac16ee3c9fd31a3463d166a
Void-CA <castilloari282@gmail.com>
2026-02-21 18:51:02 -0600

    feat(macros): First functional procedural macro

--- magic/src/lib.rs
+++ magic/src/lib.rs
@@ -1,8 +1,28 @@
-use magic_derive::MagicModel;
+pub use magic_derive::MagicModel;
 
 #[derive(MagicModel)]
 pub struct User {
     pub id: i64,
     pub name: String,
     pub email: String,
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_user_generates_new_struct() {
+        let new_user = User::new(
+            "Alice".to_string(),
+            "alice@mail.com".to_string(),
+        );
+
+        // Si la macro generó NewUser correctamente,
+        // esto debería compilar.
+        let _name = new_user.name;
+        let _email = new_user.email;
+    }
 }
