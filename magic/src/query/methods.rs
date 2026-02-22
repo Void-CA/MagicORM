8b975f8de603607f00adba629553305334f9a6a4
Void-CA <castilloari282@gmail.com>
2026-02-22 01:48:41 -0600

    feat(query): add join method and joins field to QueryBuilder for SQL query construction

--- magic/src/query/methods.rs
+++ magic/src/query/methods.rs
@@ -9,6 +9,7 @@ where
             table,
             select_columns: vec![],
             filters: vec![],
+            joins: vec![],
             order_by: None,
             limit: None,
             offset: None,
@@ -41,30 +42,13 @@ where
         self
     }
 
+    pub fn join(mut self, table: &str, on: &str, join_type: &str) -> Self {
+        self.joins.push(format!("{} JOIN {} ON {}", join_type, table, on));
+        self
+    }
+
     pub async fn fetch_all(self, pool: &sqlx::SqlitePool) -> sqlx::Result<Vec<T>> {
-        let mut sql = if self.select_columns.is_empty() {
-            format!("SELECT * FROM {}", self.table)
-        } else {
-            format!("SELECT {} FROM {}", self.select_columns.join(", "), self.table)
-        };
-
-        if !self.filters.is_empty() {
-            sql.push_str(" WHERE ");
-            sql.push_str(&self.filters.join(" AND "));
-        }
-
-        if let Some(order) = self.order_by {
-            sql.push_str(" ORDER BY ");
-            sql.push_str(&order);
-        }
-
-        if let Some(limit) = self.limit {
-            sql.push_str(&format!(" LIMIT {}", limit));
-        }
-
-        if let Some(offset) = self.offset {
-            sql.push_str(&format!(" OFFSET {}", offset));
-        }
+        let sql = self.build_sql();
 
         sqlx::query_as::<_, T>(&sql)
             .fetch_all(pool)
@@ -72,20 +56,33 @@ where
     }
 
     pub async fn fetch_one(self, pool: &sqlx::SqlitePool) -> sqlx::Result<Option<T>> {
+        let sql = self.build_sql();
+
+        sqlx::query_as::<_, T>(&sql)
+            .fetch_optional(pool)
+            .await
+    }
+
+    fn build_sql(&self) -> String {
         let mut sql = if self.select_columns.is_empty() {
             format!("SELECT * FROM {}", self.table)
         } else {
             format!("SELECT {} FROM {}", self.select_columns.join(", "), self.table)
         };
 
+        if !self.joins.is_empty() {
+            sql.push(' ');
+            sql.push_str(&self.joins.join(" "));
+        }
+
         if !self.filters.is_empty() {
             sql.push_str(" WHERE ");
             sql.push_str(&self.filters.join(" AND "));
         }
 
-        if let Some(order) = self.order_by {
+        if let Some(order) = &self.order_by {
             sql.push_str(" ORDER BY ");
-            sql.push_str(&order);
+            sql.push_str(order);
         }
 
         if let Some(limit) = self.limit {
@@ -96,8 +93,6 @@ where
             sql.push_str(&format!(" OFFSET {}", offset));
         }
 
-        sqlx::query_as::<_, T>(&sql)
-            .fetch_optional(pool)
-            .await
+        sql
     }
 }
--- magic/src/query/model.rs
+++ magic/src/query/model.rs
@@ -2,6 +2,7 @@ pub struct QueryBuilder<'a, T> {
     pub table: &'a str,
     pub select_columns: Vec<&'a str>,
     pub filters: Vec<String>,
+    pub joins: Vec<String>,
     pub order_by: Option<String>,
     pub limit: Option<u32>,
     pub offset: Option<u32>,
