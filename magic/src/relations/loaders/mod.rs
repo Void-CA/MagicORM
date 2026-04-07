b32764aaf1e12015f5e18ec1695c33c6a9fd54e7
Void-CA <castilloari282@gmail.com>
2026-04-06 20:14:59 -0600

    feat: implement lazy loading for belongs_to and has_many relations

--- magic/src/relations/loaders/mod.rs
+++ magic/src/relations/loaders/mod.rs
@@ -1,5 +1,5 @@
-pub mod has_many;
 pub mod belongs_to;
+pub mod has_many;
 
-pub use has_many::load_has_many;
-pub use belongs_to::load_belongs_to;
+pub use belongs_to::lazy::load_belongs_to;
+pub use has_many::lazy::load_has_many;
